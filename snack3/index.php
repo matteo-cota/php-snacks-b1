2e1f4051b565537026874cc244ee48d1df7162ef
matteo-cota <matteocota@outlook.it>
2024-10-04 12:49:36 +0200

    snack3

---
+++ snack3/index.php
@@ -0,0 +1,48 @@
+<?php
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    foreach ($db as $role => $people) {
+        echo "<div class='$role'>";
+        echo "<h3>" . ucfirst($role) . "</h3>";
+    }
+    ?>
+
+
+
+</body>
+
+
+</html>
