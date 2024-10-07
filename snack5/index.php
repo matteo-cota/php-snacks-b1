3e53cafeb488edde01c032b5e4c918cc560ba7d9
matteo-cota <matteocota@outlook.it>
2024-10-07 12:18:12 +0200

    snack 5

---
+++ snack5/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 5</title>
+</head>
+
+<body>
+    <form method="get" action="">
+        <label for="word">Inserisci una parola o frase:</label>
+        <input type="text" id="word" name="word" required>
+        <button type="submit">Verifica</button>
+    </form>
+
+
+</body>
+
+</html>
